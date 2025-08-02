8f7833f7d6ce1a7bcd0ec51403d2012d15936745
muruhhh <kareem_mohamed94@outlook.com>
2025-08-03 01:40:57 +0300

    feat: create Routes + Navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route, Navigate, Link } from 'react-router-dom';
+import PaginationView from './pages/PaginationView';
+import LoadMoreView from './pages/LoadMoreView';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="min-h-screen bg-gray-50 p-4">
+      <Link to="/pagination">Pagination View</Link>
+      <Link to="/load-more">Load More View</Link>
+
+      <Routes>
+        <Route path="/" element={<Navigate to="/pagination" replace />} />
+        <Route path="/pagination" element={<PaginationView />} />
+        <Route path="/load-more" element={<LoadMoreView />} />
+        <Route path="*" element={<p>404 Not Found</p>} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
