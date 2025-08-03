031641c92978b62e53a45ad5630cd90ca642a21f
muruhhh <kareem_mohamed94@outlook.com>
2025-08-03 04:42:32 +0300

    feat: fetch pokemon list from api

--- src/pages/PaginationView.tsx
+++ src/pages/PaginationView.tsx
@@ -1,9 +1,33 @@
+import { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { fetchPokemonList } from '../api/pokemon';
+import PokemonCard from '../components/PokemonCard';
+import type { PokemonListResponse } from '../types/pokemon';
+
 function PaginationView() {
   
+    const PAGE_SIZE = 20;
+
+    const [page, setPage] = useState(0);
+
+    const { data, isLoading, isError, refetch } = useQuery<PokemonListResponse>({
+        queryKey: ['pokemonList', page],
+        queryFn: () => fetchPokemonList(PAGE_SIZE, page * PAGE_SIZE),
+        keepPreviousData: true,
+    });
+
+    
   return (
     <section className="flex-col items-center py-10 px-4">
         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 gap-6 mb-10">
-            
+            {data?.results.map((pokemon, i) => (
+                <PokemonCard
+                    key={pokemon.name}
+                    name={pokemon.name}
+                    spriteUrl={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${page * PAGE_SIZE + i + 1}.png`}
+                    index={page * PAGE_SIZE + i}
+                />
+            ))}
         </div>
     </section>
   );
