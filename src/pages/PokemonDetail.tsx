95349e42795dbea5fe009015fb8817447498db96
muruhhh <kareem_mohamed94@outlook.com>
2025-08-03 05:34:28 +0300

    feat: create pokemonDetail query

--- src/pages/PokemonDetail.tsx
+++ src/pages/PokemonDetail.tsx
@@ -1,5 +1,21 @@
-export default function PokemonDetail() {
-    return(
-        <h1>Pokemon Detail Page</h1>
-    );    
-}
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import { fetchPokemonDetail } from '../api/pokemon';
+import type { PokemonDetail } from '../types/pokemon';
+
+const PokemonDetail = () => {
+  const { name } = useParams<{ name: string }>();
+
+  const { data, isLoading, isError, refetch } = useQuery<PokemonDetail>({
+    queryKey: ['pokemonDetail', name],
+    queryFn: () => fetchPokemonDetail(name!),
+    enabled: !!name,
+  });
+   
+
+  return (
+    <h1>Pokemon Detail Page</h1>
+  );
+};
+
+export default PokemonDetail;
