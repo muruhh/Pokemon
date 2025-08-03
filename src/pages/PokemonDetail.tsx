13da3dad8e40c4ae4e201163aa7830e888b7b650
muruhhh <kareem_mohamed94@outlook.com>
2025-08-03 06:35:38 +0300

    feat: create PokemonDetail header

--- src/pages/PokemonDetail.tsx
+++ src/pages/PokemonDetail.tsx
@@ -2,19 +2,19 @@ import { useParams, Link } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import { fetchPokemonDetail } from '../api/pokemon';
 import Loader from '../components/Loader';
-import type { PokemonDetail } from '../types/pokemon';
+import type { PokemonDetail as PokemonDetailType } from '../types/pokemon';
 
 const PokemonDetail = () => {
   const { name } = useParams<{ name: string }>();
 
-  const { data, isLoading, isError, refetch } = useQuery<PokemonDetail>({
+  const { data, isLoading, isError, refetch } = useQuery<PokemonDetailType>({
     queryKey: ['pokemonDetail', name],
     queryFn: () => fetchPokemonDetail(name!),
     enabled: !!name,
   });
 
   if (isLoading) return <Loader />;
-  
+
   if (isError) {
     return (
       <div className="text-center py-6">
@@ -24,29 +24,24 @@ const PokemonDetail = () => {
     );
   }
 
+  const id = data.id.toString().padStart(3, '0');
+  const gradientBg = 'bg-gradient-to-r from-purple-500 to-pink-500';
+
   return (
-    <div className="max-w-md mx-auto text-center">
-      <Link to="/pagination" className="text-sm text-blue-600 underline block mb-4 text-left">
-        ← Back
-      </Link>
+    <section className="min-h-screen py-10 px-4 flex justify-center items-start">
+      <div className="w-full max-w-3xl bg-white rounded-lg shadow overflow-hidden">
 
-      <h1 className="text-2xl font-bold capitalize mb-2">{data?.name}</h1>
+        <div className={`${gradientBg} text-white px-6 py-4 relative`}>
+          
+          <h1 className="text-3xl font-bold text-center flex items-center justify-center gap-2">
+            <svg width="32" height="32" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><defs><linearGradient id="bolt-gradient" x1="0" y1="0" x2="24" y2="24" gradientUnits="userSpaceOnUse"><stop stop-color="#fff"/><stop offset="1" stop-color="#fff"/> </linearGradient></defs><path d="M13 2L4 14H11L9 22L20 10H13L13 2Z" stroke="url(#bolt-gradient)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" fill="none"/></svg>
+            {data.name.charAt(0).toUpperCase() + data.name.slice(1)}
+          </h1>
+          <p className="text-center text-sm">#{id}</p>
+        </div>
 
-      <img
-        src={data?.sprites.front_default}
-        alt={data?.name}
-        className="w-32 h-32 mx-auto mb-4"
-      />
-
-      <div className="text-gray-700 space-y-2">
-        <p><strong>Height:</strong> {data?.height}</p>
-        <p><strong>Weight:</strong> {data?.weight}</p>
-        <p>
-          <strong>Types:</strong>{' '}
-          {data?.types.map(t => t.type.name).join(', ')}
-        </p>
       </div>
-    </div>
+    </section>
   );
 };
 
