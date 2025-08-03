64c77bb9e268fb6436daacf193ac41070791a8a5
muruhhh <kareem_mohamed94@outlook.com>
2025-08-03 08:28:28 +0300

    feat: improve loader placeholder

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,7 +1,28 @@
+import LoaderCard from "./LoaderCard";
+
 const Loader = () => (
-  <div className="text-center text-gray-600 py-8">
-    <span className="animate-pulse text-lg">Loading...</span>
-  </div>
+  <section className='min-h-screen w-full flex flex-col items-center py-10 px-4'>
+    <div className="flex-col items-center w-full max-w-7xl">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        
+        <LoaderCard />
+        <LoaderCard />
+        <LoaderCard />
+        <LoaderCard />
+
+        <LoaderCard />
+        <LoaderCard />
+        <LoaderCard />
+        <LoaderCard />
+        
+        <LoaderCard />
+        <LoaderCard />
+        <LoaderCard />
+        <LoaderCard />
+
+      </div>
+    </div>
+  </section>
 );
 
 export default Loader;
---
+++ src/components/LoaderCard.tsx
@@ -0,0 +1,9 @@
+export default function LoaderCard(){
+    return(
+        <div className="bg-white p-4 rounded shadow animate-pulse">
+          <div className="h-32 bg-gray-300 rounded mb-4"></div>
+          <div className="h-4 bg-gray-300 rounded w-3/4 mb-2"></div>
+          <div className="h-4 bg-gray-200 rounded w-1/2"></div>
+        </div>
+    )
+}
