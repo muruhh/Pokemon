80ffc6f79db8b32a84cad068cd4f81e91e5b559f
muruhhh <kareem_mohamed94@outlook.com>
2025-08-03 05:35:04 +0300

    feat: display pokemon details

--- src/pages/PokemonDetail.tsx
+++ src/pages/PokemonDetail.tsx
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import { fetchPokemonDetail } from '../api/pokemon';
 import type { PokemonDetail } from '../types/pokemon';
@@ -14,7 +14,28 @@ const PokemonDetail = () => {
    
 
   return (
-    <h1>Pokemon Detail Page</h1>
+    <div className="max-w-md mx-auto text-center">
+      <Link to="/pagination" className="text-sm text-blue-600 underline block mb-4 text-left">
+        ← Back
+      </Link>
+
+      <h1 className="text-2xl font-bold capitalize mb-2">{data?.name}</h1>
+
+      <img
+        src={data?.sprites.front_default}
+        alt={data?.name}
+        className="w-32 h-32 mx-auto mb-4"
+      />
+
+      <div className="text-gray-700 space-y-2">
+        <p><strong>Height:</strong> {data?.height}</p>
+        <p><strong>Weight:</strong> {data?.weight}</p>
+        <p>
+          <strong>Types:</strong>{' '}
+          {data?.types.map(t => t.type.name).join(', ')}
+        </p>
+      </div>
+    </div>
   );
 };
 
