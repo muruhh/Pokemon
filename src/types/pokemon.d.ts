23406d69179bcfed6dee4fa52b12db2ae372e288
muruhhh <kareem_mohamed94@outlook.com>
2025-08-03 05:31:32 +0300

    feat: handle pokemon detail interface

--- src/types/pokemon.d.ts
+++ src/types/pokemon.d.ts
@@ -4,3 +4,18 @@ export interface PokemonListResponse {
   previous: string | null;
   results: { name: string; url: string }[];
 }
+
+export interface PokemonDetail {
+  id: number;
+  name: string;
+  height: number;
+  weight: number;
+  sprites: {
+    front_default: string;
+  };
+  types: {
+    type: {
+      name: string;
+    };
+  }[];
+}
