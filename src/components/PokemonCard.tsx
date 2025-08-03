d7547bb6eca98fff95f29cdb9f0ae169ed3efea4
muruhhh <kareem_mohamed94@outlook.com>
2025-08-03 08:07:33 +0300

    style: enhance pokemon card image view

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -13,7 +13,7 @@ const PokemonCard = ({ name, spriteUrl, index }: Props) => {
     <div className="bg-white rounded shadow p-4 text-center">
         <Link to={`/pokemon/${name}`} >
             <div className="bg-gray-100">
-                <img src={spriteUrl} alt={name} className="mx-auto h-32 object-contain" />
+                <img src={spriteUrl} alt={name} className="mx-auto h-50 object-contain" />
             </div>
             <h2 className="capitalize font-medium text-lg pt-3">{name}</h2>
             <p className="text-sm text-gray-400">{paddedIndex}</p>
