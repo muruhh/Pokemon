ba48a68f7d5be92a984aeb35ef2819d158e32fa2
muruhhh <kareem_mohamed94@outlook.com>
2025-08-03 09:03:53 +0300

    fix: add PokemonDetail types

--- src/types/pokemon.d.ts
+++ src/types/pokemon.d.ts
@@ -10,11 +10,29 @@ export interface PokemonDetail {
   name: string;
   height: number;
   weight: number;
+  base_experience: number;
   sprites: {
     front_default: string;
+    other?: {
+      ['official-artwork']?: {
+        front_default?: string;
+      };
+    };
   };
   types: {
     type: {
+      name: string;
+    };
+  }[];
+  abilities: {
+    is_hidden: boolean;
+    ability: {
+      name: string;
+    };
+  }[];
+  stats: {
+    base_stat: number;
+    stat: {
       name: string;
     };
   }[];
